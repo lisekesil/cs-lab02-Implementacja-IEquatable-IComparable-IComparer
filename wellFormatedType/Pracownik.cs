bd47e7fda14a555308330d5e535ca0a61f459911
Wojtek Lisowicz <lisekmadridista@gmail.com>
2020-12-04 13:08:36 +0100

    krok2

--- wellFormatedType/Pracownik.cs
+++ wellFormatedType/Pracownik.cs
@@ -1,10 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Text;
 
 namespace wellFormatedType
 {
-    class Pracownik
+    class Pracownik : IEquatable<Pracownik>, IComparable<Pracownik>
     {
         private string _nazwisko;
         private DateTime _dataZatrudnienia;
@@ -55,5 +56,83 @@ namespace wellFormatedType
         }
 
         public override string ToString() => $"({Nazwisko}, {DataZatrudnienia:d MMM yyyy} ({CzasZatrudnienia}), {Wynagrodzenie} PLN)";
+
+        public bool Equals( Pracownik other)
+        {
+            if(other == null)
+            {
+                return false;
+            }
+            if (Object.ReferenceEquals(this, other)) return true;
+
+            return (other.Nazwisko == Nazwisko && other.Wynagrodzenie == Wynagrodzenie && other.DataZatrudnienia == DataZatrudnienia);
+
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is Pracownik)
+            {
+                return Equals((Pracownik)obj);
+            }
+            else
+                return false;
+        }
+
+        public override int GetHashCode() => (Nazwisko, DataZatrudnienia, Wynagrodzenie).GetHashCode();
+        
+        public static bool Equals(Pracownik p1, Pracownik p2)
+        {
+            if ((p1 is null) && (p2 is null)) return true;
+            if (p1 is null) return false;
+
+            return p1.Equals(p2);
+        }
+
+        public static bool operator ==(Pracownik p1, Pracownik p2) => Equals(p1, p2);
+        public static bool operator !=(Pracownik p1, Pracownik p2) => !(p1 == p2);
+        public int CompareTo(Pracownik other)
+        {
+            if (other == null) return 1;
+
+            /*
+             if (other is null) return 1; // w C#: null jest najmniejszą wartością (`this > null`)
+             if (this.Equals(other)) return 0; //zgodność z Equals (`this == other`)
+
+             if (this.Nazwisko != other.Nazwisko)
+                return this.Nazwisko.CompareTo(other.Nazwisko);
+    
+                // ponieważ nazwiska równe, porządkujemy wg daty
+             if (!this.DataZatrudnienia.Equals(other.DataZatrudnienia)) // != zamiast !Equals
+                  return this.DataZatrudnienia.CompareTo(other.DataZatrudnienia);
+
+             // ponieważ nazwiska równe i daty równe, porządkujemy wg wynagrodzenia
+             return this.Wynagrodzenie.CompareTo(other.Wynagrodzenie);
+             */
+
+            if (Nazwisko.CompareTo(other.Nazwisko) > 0) {
+                return 1;
+            } else if(Nazwisko.CompareTo(other.Nazwisko) == 0)
+            {
+                if(DataZatrudnienia.CompareTo(other.DataZatrudnienia) > 0)
+                {
+                    return 1;
+                } else if(DataZatrudnienia.CompareTo(other.DataZatrudnienia) == 0)
+                {
+                    if(Wynagrodzenie.CompareTo(other.Wynagrodzenie) > 0)
+                    {
+                        return 1;
+                    } else if(Wynagrodzenie.CompareTo(other.Wynagrodzenie) == 0)
+                    {
+                        return 0;
+                    }
+                    return -1;
+                }
+                return -1;
+            }
+            return -1;
+
+        }
+
     }
 }
--- wellFormatedType/Program.cs
+++ wellFormatedType/Program.cs
@@ -1,19 +1,30 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 
 namespace wellFormatedType
 {
-    class Program
+    class Program 
     {
         static void Main(string[] args)
         {
-            var p1 = new Pracownik();
-            p1.Wynagrodzenie = 100;
-            p1.Nazwisko = "   Molenda  ";
-            p1.DataZatrudnienia = new DateTime(2010, 10, 01);
-            Console.WriteLine(p1);
-
+            var p1 = new Pracownik("Molenda", new DateTime(2011, 8,9), 400);
             var p2 = new Pracownik("Abacki", new DateTime(2011, 8, 9), 200);
-            Console.WriteLine(p2);
+            var p3 = new Pracownik("Abacki", new DateTime(2016, 5, 3), 300);
+            var p33 = new Pracownik("Abacki", new DateTime(2016, 5, 3), 400);
+            var p34 = new Pracownik("Abacki", new DateTime(2016, 5, 3), 100);
+            var p4 = new Pracownik("Nowak", new DateTime(2017, 4, 6), 500);
+            var p5 = new Pracownik("Kowalski", new DateTime(2015, 12, 3), 500);
+
+            List<Pracownik> pracownicy = new List<Pracownik> { p1, p2, p3, p4, p5, p33,p34 };
+            pracownicy.Sort();
+            foreach(var item in pracownicy)
+            {
+                Console.WriteLine(item);
+            }
+            
         }
+
+ 
     }
 }
